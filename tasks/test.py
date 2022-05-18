dd0b50ae6b4f0ad20ebca8cd157af49bdef3a907
Trey Stafford <trey.stafford@nsidc.org>
2022-05-18 15:54:53 -0600

    Add mypy & typecheck invoke task

--- tasks/test.py
+++ tasks/test.py
@@ -7,3 +7,23 @@ from .util import PROJECT_DIR, print_and_run
 def lint(ctx):
     """Run flake8 linting."""
     print_and_run(f'flake8 {PROJECT_DIR}')
+
+
+@task(aliases=['mypy'])
+def typecheck(ctx):
+    """Run mypy typechecking."""
+    print_and_run(
+        f'mypy --config-file=.mypy.ini {PROJECT_DIR}/',
+        pty=True,
+    )
+
+    print('🎉🦆 Type checking passed.')
+
+
+@task(pre=[
+    lint,
+    typecheck,
+], default=True)
+def all(ctx):
+    """Run all of the tests."""
+    ...
