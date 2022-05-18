e4ebb6007913035f789e945ec5ccee9a8ed6a505
Trey Stafford <trey.stafford@nsidc.org>
2022-05-18 09:08:07 -0600

    Add invoke tasks for linting and formatting code

---
+++ tasks/util.py
@@ -0,0 +1,15 @@
+import inspect
+from pathlib import Path
+
+from invoke import run
+
+PROJECT_DIR = Path(__file__).parent.parent
+
+
+def print_and_run(cmd, **run_kwargs):
+    print(cmd)
+    kwargs = {
+        'pty': True,
+        **run_kwargs,
+    }
+    return run(cmd, **kwargs)
