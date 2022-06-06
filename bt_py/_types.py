18006743d6f784358cb61ec3aadb8761674027ea
Trey Stafford <trey.stafford@nsidc.org>
2022-06-06 09:15:45 -0600

    Fixup key name in `Variables` typeddict

--- bt_py/_types.py
+++ bt_py/_types.py
@@ -57,7 +57,7 @@ class Variables(TypedDict):
     adoff: float
     itp: list[float]  # len 2
     itp2: list[float]  # len 2
-    radlen: float
+    radlen1: float
     radlen2: float
     radoff1: float
     radoff2: float
