304e52526276e093c7d38b163f9c96dcea5d7234
Trey Stafford <trey.stafford@nsidc.org>
2022-06-09 14:38:41 -0600

    Fixup typos (`aui_si25` -> `au_si25`)

--- cdr_amsr2/tests/test_fetch.py
+++ cdr_amsr2/tests/test_fetch.py
@@ -2,11 +2,11 @@ import numpy as np
 import xarray as xr
 from xarray.testing import assert_equal
 
-from cdr_amsr2.fetch import aui_si25
+from cdr_amsr2.fetch import au_si25
 
 
 def test__normalize_au_si25_tbs():
-    mock_aui_si25_data_fields = xr.Dataset(
+    mock_au_si25_data_fields = xr.Dataset(
         data_vars={
             'SI_25km_NH_06H_DAY': ('x', np.arange(0, 5)),
             'SI_25km_NH_89V_DAY': ('x', np.arange(5, 10)),
@@ -19,8 +19,8 @@ def test__normalize_au_si25_tbs():
             'v89': ('x', np.arange(5, 10)),
         },
     )
-    actual = aui_si25._normalize_au_si25_tbs(
-        mock_aui_si25_data_fields,
+    actual = au_si25._normalize_au_si25_tbs(
+        mock_au_si25_data_fields,
     )
 
     assert_equal(actual, expected)
