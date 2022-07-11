df51f1e4d4460bf07e18188b768d9cfc74d2a7f2
Trey Stafford <trey.stafford@nsidc.org>
2022-07-11 12:04:33 -0600

    Add type for valid sats

--- cdr_amsr2/_types.py
+++ cdr_amsr2/_types.py
@@ -1,3 +1,8 @@
 from typing import Literal
 
 Hemisphere = Literal['north', 'south']
+
+# TODO: can we make the algorithms satellite-independent? Currently code
+# branches in e.g., `compute_bt_ic` that rely on exact string matches for some
+# behavior/config.
+ValidSatellites = Literal['u2', '17', '18', '00']
