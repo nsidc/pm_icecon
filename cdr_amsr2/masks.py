36f5552874edacc573ded91c3723a509a5404030
Trey Stafford <trey.stafford@nsidc.org>
2022-08-05 17:11:13 -0600

    DRY out `get_ps_valid_ice_mask`

--- cdr_amsr2/masks.py
+++ cdr_amsr2/masks.py
@@ -115,22 +115,36 @@ def get_e2n625_land_mask() -> npt.NDArray[np.bool_]:
 
 
 # TODO: rename to indicate this is derived from SST?
-def get_ps25_valid_ice_mask(
+def get_ps_valid_ice_mask(
     *,
     hemisphere: Hemisphere,
     date: dt.date,
+    resolution: str,
 ) -> npt.NDArray[np.bool_]:
-    """Read and return the polar stereo 25km valid ice mask."""
+    """Read and return the polar stereo valid ice mask."""
+    print(f'Reading valid ice mask for PS{hemisphere[0].upper()} {resolution}km grid')
     if hemisphere == 'north':
-        print('Reading valid ice mask for PSN 25km grid')
-        sst_fn = (
-            PACKAGE_DIR
-            / '../legacy'
-            / f'SB2_NRT_programs/ANCILLARY/np_sect_sst1_sst2_mask_{date:%m}.int'
-        ).resolve()
-        sst_mask = np.fromfile(sst_fn, dtype=np.int16).reshape(448, 304)
+        if resolution == '25':
+            sst_fn = (
+                PACKAGE_DIR
+                / '../legacy'
+                / f'SB2_NRT_programs/ANCILLARY/np_sect_sst1_sst2_mask_{date:%m}.int'
+            ).resolve()
+            sst_mask = np.fromfile(sst_fn, dtype=np.int16).reshape(448, 304)
+        elif resolution == '12':
+            mask_fn = Path(
+                '/share/apps/amsr2-cdr/cdr_testdata/btequiv_psn12.5/'
+                f'bt_validmask_psn12.5km_{date:%m}.dat'
+            )
+
+            # The valid ice array has values of 0, 4, 24, and 32. It looks like values
+            # of 4 represent areas of valid ice.
+            sst_mask = np.fromfile(mask_fn, dtype=np.int16).reshape(896, 608)
     else:
-        print('Reading valid ice mask for PSN 25km grid')
+        if resolution != '25':
+            raise NotImplementedError(
+                'We have not yet created a SH PS 25 valid ice mask.'
+            )
         sst_fn = Path(
             '/share/apps/amsr2-cdr'
             '/cdr_testdata/bt_goddard_ANCILLARY'
@@ -141,71 +155,3 @@ def get_ps25_valid_ice_mask(
     is_high_sst = sst_mask == 24
 
     return is_high_sst
-
-
-def get_ps12_valid_ice_mask(
-    hemisphere: Hemisphere,
-    date: dt.date,
-) -> npt.NDArray[np.bool_]:
-    """Read and return the polar stereo 12km valid ice mask.
-
-    scotts said on 2022-07-11:
-
-    I've created NH 12.5km "bt_equiv" files for use with a version of the
-    bootstrap algorithm running in the Northern Hemisphere on the 12.5km polar
-    stereo grid.I placed these in:
-
-    * /share/apps/amsr2-cdr/cdr_testdata/btequiv_psn12.5/
-
-    * And if you do a 'tree' command on the new directory, tree btequiv_psn12.5/
-      from the cdr_testdate/ dir, you get:
-
-    btequiv_psn12.5/
-    ├── bt_landequiv_psn12.5km.dat     # a landmask with the same
-                                       #  encoding as the BT mask
-    ├── bt_poleequiv_psn12.5km.dat     # a pole-hole mask with the same encoding
-                                       #  as the BT polehole mask
-    ├── bt_validmask_psn12.5km_01.dat  # 12 monthly valid ice masks
-    ├── bt_validmask_psn12.5km_02.dat
-    ├── bt_validmask_psn12.5km_03.dat
-    ├── bt_validmask_psn12.5km_04.dat
-    ├── bt_validmask_psn12.5km_05.dat
-    ├── bt_validmask_psn12.5km_06.dat
-    ├── bt_validmask_psn12.5km_07.dat
-    ├── bt_validmask_psn12.5km_08.dat
-    ├── bt_validmask_psn12.5km_09.dat
-    ├── bt_validmask_psn12.5km_10.dat
-    ├── bt_validmask_psn12.5km_11.dat
-    └── bt_validmask_psn12.5km_12.dat
-    0 directories, 14 files
-
-    All of these are raw binary files that are 608x896 2-byte (16-bit) integers,
-    i.e. numpy dtype "np.int16"
-    """
-    if hemisphere == 'south':
-        raise NotImplementedError('We have not yet created a ps12 SH valid ice mask.')
-
-    mask_fn = Path(
-        '/share/apps/amsr2-cdr/cdr_testdata/btequiv_psn12.5/'
-        f'bt_validmask_psn12.5km_{date:%m}.dat'
-    )
-
-    # The valid ice array has values of 0, 4, 24, and 32. It looks like values
-    # of 4 represent areas of valid ice.
-    valid_ice_arr = np.fromfile(mask_fn, dtype=np.int16).reshape(896, 608)
-
-    # TODO: this is the same as the 25km mask. DRY it out!
-    return valid_ice_arr == 24
-
-
-def get_ps_valid_ice_mask(
-    hemisphere: Hemisphere,
-    date: dt.date,
-    resolution: str,
-) -> npt.NDArray[np.bool_]:
-    if resolution == '12':
-        return get_ps12_valid_ice_mask(hemisphere=hemisphere, date=date)
-    elif resolution == '25':
-        return get_ps25_valid_ice_mask(hemisphere=hemisphere, date=date)
-    else:
-        raise NotImplementedError(f'No valid ice masks for PS {resolution} available.')
