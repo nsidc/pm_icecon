007729449bc3ef6d2aa8a0717443145729ab8f99
Trey Stafford <trey.stafford@nsidc.org>
2022-08-23 11:15:56 -0600

    Move nt regression data to share location
    TODO: confirm this is actually output we've vetted

--- cdr_amsr2/tests/regression/test_nt.py
+++ cdr_amsr2/tests/regression/test_nt.py
@@ -1,17 +1,14 @@
 import numpy as np
 from numpy.testing import assert_equal
 
-# from cdr_amsr2.tests.regression.util import REGRESSION_DATA_DIR
-from cdr_amsr2.constants import PACKAGE_DIR
 from cdr_amsr2.nt.api import original_example
+from cdr_amsr2.tests.regression.util import REGRESSION_DATA_DIR
 
 
 def test_nt_f17_regression():
     """Regressi5on test for NT F17 output."""
     regression_data = np.fromfile(
-        # TODO: move regression data to regression data dir location
-        # REGRESSION_DATA_DIR / 'nt_f17_regression/...',
-        PACKAGE_DIR / 'nt' / 'nt_sample_nh.dat',
+        REGRESSION_DATA_DIR / 'nt_f17_regression' / 'nt_sample_nh.dat',
         dtype=np.int16,
     ).reshape((448, 304))
 
