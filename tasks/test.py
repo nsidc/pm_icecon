aba3baf06033709c19b192fa83fbaaed4ab2f435
Trey Stafford <trey.stafford@nsidc.org>
2022-08-24 16:46:49 -0600

    Modify PYTHONPATH for mypy task
    attempt to fix error w/ circle
    
    https://app.circleci.com/pipelines/bitbucket/nsidc/cdr_amsr2/175/workflows/8507e54b-ac9a-495d-9a12-8435f698d700/jobs/175

--- tasks/test.py
+++ tasks/test.py
@@ -16,7 +16,10 @@ def typecheck(ctx):
     """Run mypy typechecking."""
     mypy_cfg_path = PROJECT_DIR / '.mypy.ini'
     print_and_run(
-        (f'mypy --config-file={mypy_cfg_path}' f' {PROJECT_DIR}/'),
+        (
+            f'PYTHONPATH={PROJECT_DIR} mypy --config-file={mypy_cfg_path}'
+            f' {PROJECT_DIR}/'
+        ),
         pty=True,
     )
 
