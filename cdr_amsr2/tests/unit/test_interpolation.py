f72236ae011fae39c3469dc21917347fa022b75d
Trey Stafford <trey.stafford@nsidc.org>
2022-09-14 16:05:05 -0600

    Add unit test for interpolation code

---
+++ cdr_amsr2/tests/unit/test_interpolation.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numpy.testing import assert_almost_equal
+
+from cdr_amsr2.interpolation import spatial_interp_tbs
+
+
+def test_spatial_interp_tbs():
+    example_data = np.array([
+        [3, 2, 1],
+        [1, 0, 3],
+        [9, 2, 3],
+    ], dtype=np.float32)
+
+    adjacent = np.array([2, 1, 3, 2])
+    diagonal = np.array([3, 1, 9, 3])
+    diagonal_weighted = diagonal * 0.707
+
+    count = len(adjacent) + (len(diagonal) * 0.707)
+
+    expected_value = (
+        np.concatenate([adjacent, diagonal_weighted]).sum()
+        / count
+    )
+
+    expected = np.array([
+        [3, 2, 1],
+        [1, expected_value, 3],
+        [9, 2, 3],
+    ], dtype=np.float32)
+
+    actual = spatial_interp_tbs({'chan': example_data})['chan']
+
+    assert_almost_equal(expected, actual, decimal=6)
