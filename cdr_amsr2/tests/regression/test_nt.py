ab9eb0446c14bf4f4928c13dfe5b27a2a2ca2456
Scott <scotts@nsidc.org>
2022-09-20 14:17:19 -0600

    clean up adjustment scaffolding

--- cdr_amsr2/tests/regression/test_nt.py
+++ cdr_amsr2/tests/regression/test_nt.py
@@ -14,79 +14,20 @@ def test_nt_f17_regressions():
     """Regression test for NT F17 output."""
     for hemisphere in get_args(Hemisphere):
 
-        if hemisphere == 'north':
-            """
-            regression_data = np.fromfile(
-                REGRESSION_DATA_DIR
-                / 'nt_f17_regression'
-                / f'{hemisphere[0].upper()}H_f17_20180101_int16.dat',
-                dtype=np.int16,
-            ).reshape(get_ps25_grid_shape(hemisphere=hemisphere))
-
-            # Clamp concentrations to a max of 100
-            regression_data[(regression_data > 100) & (regression_data < 200)] = 100
-
-            # Scale down by 10
-            regression_data = regression_data / 10  # type: ignore
-            """
-
-            regression_ds = xr.open_dataset(
-                REGRESSION_DATA_DIR
-                / 'nt_f17_regression'
-                / f'{hemisphere[0].upper()}H_f17_20180101_regression.nc',
-            )
-            regression_data = regression_ds.conc.data
-
-        elif hemisphere == 'south':
-            regression_ds = xr.open_dataset(
-                REGRESSION_DATA_DIR
-                / 'nt_f17_regression'
-                / f'{hemisphere[0].upper()}H_f17_20180101_regression.nc',
-            )
-            regression_data = regression_ds.conc.data
+        regression_ds = xr.open_dataset(
+            REGRESSION_DATA_DIR
+            / 'nt_f17_regression'
+            / f'{hemisphere[0].upper()}H_f17_20180101_regression.nc',
+        )
+        regression_data = regression_ds.conc.data
 
         actual_ds = original_example(hemisphere=hemisphere)
+        actual_data = actual_ds.conc.data
 
-        # assert that the only differences are land values. For some reason
-        # (TODO - investigate), the regression data does not have the southern
-        # tip of south america in the data.
-        if hemisphere == 'south':
-            print('Testing test_nt_f17_regression: south')
-            diff = np.abs(regression_data - actual_ds.conc.data)
-            meaningful_diff = diff > 0.1
-            try:
-                assert np.all(diff[meaningful_diff] == 254)
-            except AssertionError as e:
-                print('Failed test_nt_f17_regression (south)')
-
-                # ofn = f'test_actual_conc_data_{hemisphere}.dat'
-                # actual_ds.conc.data.tofile(ofn)
-                # print(f'Wrote: {ofn}  {actual_ds.conc.data.dtype}  {actual_ds.conc.data.shape}')
-
-                # ofn = f'{hemisphere[0].upper()}H_f17_20180101_regression.nc'
-                # actual_ds.to_netcdf(ofn)
-                # print(f'Wrote: {ofn}')
-
-                print(f'{e}')
-                print(f'hemisphere: {hemisphere}')
-                # raise e
-        else:
-            print('Testing test_nt_f17_regression: north')
-            diff = np.abs(regression_data - actual_ds.conc.data)
-            meaningful_diff = diff > 0.1
-            try:
-                assert_almost_equal(regression_data, actual_ds.conc.data, decimal=1)
-            except AssertionError as e:
-                print('Failed test_nt_f17_regression (north)')
-
-                # ofn = f'test_actual_conc_data_{hemisphere}.dat'
-                # actual_ds.conc.data.tofile(ofn)
-                # print(f'Wrote: {ofn}  {actual_ds.conc.data.dtype}  {actual_ds.conc.data.shape}')
-
-                # ofn = f'{hemisphere[0].upper()}H_f17_20180101_regression.nc'
-                # actual_ds.to_netcdf(ofn)
-                # print(f'Wrote: {ofn}')
-
-                print(f'{e}')
-                print(f'hemisphere: {hemisphere}')
-                raise e
+        diff = np.abs(regression_data - actual_data)
+        meaningful_diff = diff > 0.1
+        assert_almost_equal(
+            regression_data,
+            actual_data,
+            decimal=1,
+        )
