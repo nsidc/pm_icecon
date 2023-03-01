8f582933967ee6358c91d7225ae23bebe4d10dda
Trey Stafford <trey.stafford@colorado.edu>
2023-03-01 16:43:49 -0700

    Add TODO

--- pm_icecon/constants.py
+++ pm_icecon/constants.py
@@ -10,5 +10,7 @@ NSIDC_NFS_SHARE_DIR = Path('/share/apps/amsr2-cdr')
 CDR_DATA_DIR = NSIDC_NFS_SHARE_DIR / 'cdr_data'
 BOOTSTRAP_MASKS_DIR = NSIDC_NFS_SHARE_DIR / 'bootstrap_masks'
 # Contains regression data, ancillary data files (masks), etc.
+# TODO: move ancillary files out of `cdr_testdata` dir? On the VMs, the
+# ancillary nasateam files in cdr_testdata are also in the `NSIDC_NFS_SHARE_DIR`
 CDR_TESTDATA_DIR = NSIDC_NFS_SHARE_DIR / 'cdr_testdata'
 BT_GODDARD_ANCILLARY_DIR = CDR_TESTDATA_DIR / 'bt_goddard_ANCILLARY'
