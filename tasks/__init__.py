7de4a6eab1de593807391d3c713b02090cc0d5ff
Scott <scotts@nsidc.org>
2023-07-03 20:10:28 -0600

    revert Collections change

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,10 +1,8 @@
-# vim coc labels this an error
-# from invoke import Collection
-from invoke.collection import Collection
+from invoke import Collection
 
 from . import format as _format
 from . import test
 
 ns = Collection()
-ns.add_collection(Collection(_format))
-ns.add_collection(Collection(test))
+ns.add_collection(_format)
+ns.add_collection(test)
