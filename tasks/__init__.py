d56221761191afd12767b346a1d5e441633623df
Scott <scotts@nsidc.org>
2023-07-03 20:07:52 -0600

    update Collections in __init__.py

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,8 +1,10 @@
-from invoke import Collection
+# vim coc labels this an error
+# from invoke import Collection
+from invoke.collection import Collection
 
 from . import format as _format
 from . import test
 
 ns = Collection()
-ns.add_collection(_format)
-ns.add_collection(test)
+ns.add_collection(Collection(_format))
+ns.add_collection(Collection(test))
