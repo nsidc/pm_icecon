04fcc9cbfe3d14f0e25a97d592d13c7859ab0528
Scott <scotts@nsidc.org>
2023-07-13 22:01:26 -0600

    add a script to run all the CircleCI checks *before* uploading to the repo

---
+++ run_ci_checks__pm_icecon.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# These are the checks that Circle CI runs when you upload a branch
+
+echo "Run flake8..."
+flake8 --exclude nt_tiepoint_generation/
+echo " "
+
+echo "Run mypy..."
+mypy --config-file=.mypy.ini .
+echo " "
+
+echo "Run vulture..."
+vulture --exclude tasks/,nt_tiepoint_generation/,pm_icecon/**/_types.py,pm_icecon/nt/_types.py,pm_icecon/config/models/base_model.py,pm_icecon/config/models/__init__.py .
+echo " "
+
+echo "Run isort..."
+isort --check-only .
+echo " "
+
+echo "Run black..."
+black --exclude nt_tiepoint_generation/ --check .
+echo " "
