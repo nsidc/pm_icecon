aaf9162ff7c930544b694164d9436e1947c7c8bf
Scott <scotts@nsidc.org>
2023-09-26 15:57:17 -0600

    remove vulture-detected code

--- pm_icecon/bt/_types.py
+++ pm_icecon/bt/_types.py
@@ -1,10 +1,6 @@
 # For Pydantic ~1.9
 from typing import NewType, TypedDict
 
-# For Pydantic ~2.0
-# from typing import NewType
-# from typing_extensions import TypedDict
-
 # A tie point is a "canonical" brightness temperature for a given channel and
 # H2O-phase. For bootstrap, the 3 relevant channels -- 19H, 37H, and 37V -- each
 # have both an "ice" tiepoint and a "water" tiepoint. These tiepoints are
@@ -16,32 +12,6 @@ Tiepoint = NewType('Tiepoint', float)
 # values with those water tie points where the pixel is calculated to have zero
 # percent sea ice.
 TiepointSet = tuple[Tiepoint, Tiepoint]
-
-
-""" Removed because of vulture
-class RawFns(TypedDict):
-    # maps strings to strings representing filepaths.
-    h37: str
-    land: str
-    nphole: str
-    v19: str
-    v22: str
-    v37: str
-"""
-
-
-""" Removed because of vulture
-# Dict returned by `ret_para_nsb2`
-class ParaVals(TypedDict):
-    iceline: list[float]  # len 2
-    itp: list[float]  # len 2
-    lnchk: float
-    lnline: list[float]  # len 2
-    wintrc: float
-    wslope: float
-    wtp: list[float]  # len 2
-    wxlimt: float
-"""
 
 
 class Line(TypedDict):
