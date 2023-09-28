b7d7181efd7c9202e837ec3de215f300f91dd6c7
Trey Stafford <trey.stafford@colorado.edu>
2023-09-28 16:27:27 -0600

    Override values in creation of `A2L1C_NORTH_PARAMS`

--- pm_icecon/bt/params/a2l1c.py
+++ pm_icecon/bt/params/a2l1c.py
@@ -1,12 +1,7 @@
-"""a2l1c.py: bootstrap parameters file.
+"""Bootstrap parameters for AMSR2 CETB product.
 
-Bootstrap parameters for use with AMSR2 from new CETB product,
-  i.e. either NSIDC-0763 or NSIDC-0630 version 2
-
-Parameters were originally provided by 'ret_parameters_amsru2.f', except:
-  - new wxlimt values were calculated by during Spring 2023 NSIDC
-    investigation of weather effects for the NOAA CDR version 5 product
-  - Note: Southern hemisphere wxlimt goes from 18.596 to 14.3
+Bootstrap parameters for use with AMSR2 from new CETB product, i.e. either
+NSIDC-0763 or NSIDC-0630 version 2.
 """
 
 from pm_icecon.bt._types import Line, Tiepoint
@@ -16,7 +11,8 @@ from pm_icecon.config.models.bt import (
     WeatherFilterParamsForSeason,
 )
 
-A2L1C_NORTH_PARAMS = dict(
+# Prameters as provided by `ret_parameters_amsru2.f`
+_ret_parameters_amsru2_f_params = dict(
     vh37_params=TbSetParams(
         water_tie_point_set=(Tiepoint(207.2), Tiepoint(131.9)),
         ice_tie_point_set=(Tiepoint(256.3), Tiepoint(241.2)),
@@ -36,8 +32,8 @@ A2L1C_NORTH_PARAMS = dict(
             weather_filter_params=WeatherFilterParams(
                 wintrc=82.71,
                 wslope=0.5352,
-                # wxlimt=23.34,  # Original value
-                wxlimt=21.7,  # Median of calc'd wx coefs
+                # Original value given in `ret_parameters_amsru2.f`
+                wxlimt=23.34,
             ),
         ),
         # Oct. 16 through May
@@ -48,9 +44,41 @@ A2L1C_NORTH_PARAMS = dict(
             weather_filter_params=WeatherFilterParams(
                 wintrc=84.73,
                 wslope=0.5352,
-                # wxlimt=18.39,  # Original value
-                wxlimt=13.7,  # Median of calc'd wx coefs
+                # Original value given in `ret_parameters_amsru2.f`
+                wxlimt=18.39,
             ),
         ),
     ],
 )
+
+
+# Parameters were originally provided by 'ret_parameters_amsru2.f', except:
+#   - new wxlimt values were calculated during Spring 2023 NSIDC
+#     investigation of weather effects for the NOAA CDR version 5 product
+A2L1C_NORTH_PARAMS = _ret_parameters_amsru2_f_params.copy()
+A2L1C_NORTH_PARAMS['weather_filter_seasons'] = [
+    # June through October 15
+    WeatherFilterParamsForSeason(
+        start_month=6,
+        end_month=10,
+        end_day=15,
+        weather_filter_params=WeatherFilterParams(
+            wintrc=82.71,
+            wslope=0.5352,
+            # Median of calc'd wx coefs
+            wxlimt=21.7,
+        ),
+    ),
+    # Oct. 16 through May
+    WeatherFilterParamsForSeason(
+        start_month=10,
+        start_day=16,
+        end_month=5,
+        weather_filter_params=WeatherFilterParams(
+            wintrc=84.73,
+            wslope=0.5352,
+            # Median of calc'd wx coefs
+            wxlimt=13.7,
+        ),
+    ),
+]
