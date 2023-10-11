06936c4394f16ecdd66f653b14e6c65c26f903e1
Trey Stafford <trey.stafford@colorado.edu>
2023-10-11 13:20:23 -0600

    Remove unnecessary try/except blocks that skip tests

--- pm_icecon/tests/integration/test_paramfiles.py
+++ pm_icecon/tests/integration/test_paramfiles.py
@@ -26,15 +26,12 @@ def test_get_bootstrap_fields():
     satellite = 'amsr2'
     gridid = 'e2n25'
 
-    try:
-        bt_field_dict = get_bootstrap_fields(
-            date=date,
-            satellite=satellite,
-            gridid=gridid,
-        )
-        assert 'invalid_ice_mask' in bt_field_dict.keys()
-    except FileNotFoundError:
-        print('Skipping test_get_bootstrap_params() because file not found')
+    bt_field_dict = get_bootstrap_fields(
+        date=date,
+        satellite=satellite,
+        gridid=gridid,
+    )
+    assert 'invalid_ice_mask' in bt_field_dict.keys()
 
 
 def test_convert_to_pmicecon_bt_params():
@@ -48,15 +45,12 @@ def test_convert_to_pmicecon_bt_params():
         satellite=satellite,
         gridid=gridid,
     )
-    try:
-        bt_field_dict = get_bootstrap_fields(
-            date=date,
-            satellite=satellite,
-            gridid=gridid,
-        )
-        oldstyle_params = convert_to_pmicecon_bt_params(
-            hemisphere, bt_parameters, bt_field_dict
-        )
-        assert oldstyle_params is not None
-    except FileNotFoundError:
-        print('Skipping test_get_bootstrap_params() because file not found')
+    bt_field_dict = get_bootstrap_fields(
+        date=date,
+        satellite=satellite,
+        gridid=gridid,
+    )
+    oldstyle_params = convert_to_pmicecon_bt_params(
+        hemisphere, bt_parameters, bt_field_dict
+    )
+    assert oldstyle_params is not None
